50372bbc78ca46be1c07fe070938373fb3893651
Ignacio Pedrosa <ignapedrosa@gmail.com>
2021-11-04 16:15:19 -0300

    Agregar registro

--- main.js
+++ main.js
@@ -1,30 +1,144 @@
+class Usuario {
 
+    constructor(user, pass) {
+        this.user = user;
+        this.pass = pass;
+    }
 
-class Persona{
+}
 
-    constructor (nombre,edad,frase){
-
-    this.nombre = nombre;
-    this.edad = edad;
-    this.frase = frase;
+function TomarDatos() {
     
+
+  
+    let user = document.getElementById("inputUser").value;
+    let pass = document.getElementById("inputPassword").value;
+    UserLoggin(user,pass);
 }
 
-hablar() {
-    console.log('Hola soy ' + this.nombre + ' tengo '+this.edad +' años. Y me gusta mucho la frase "'+this.frase+'"')
+
+function UserLoggin(usuario, password) {
+
+    if (!localStorage.getItem("listaUsuarios")) {
+        alert('No existen usuarioss')
+    }
+
+    else {
+
+    
+        let almacenados = JSON.parse(localStorage.getItem("listaUsuarios"));
+        let encontrado = false;
+        
+
+        let i = 0;
+
+        
+        while (!encontrado && i != almacenados.length) {
+
+
+            if (almacenados[i].user == usuario && almacenados[i].pass == password) {
+                encontrado = almacenados[i];
+    
+            }
+    
+            i++;
+    
+        }
+
+        if (!encontrado) {
+            alert('Usuario y/o contraseña incorrectas')
+            
+        }
+        else{
+
+            alert('¡Hola '+usuario+'!')
+        }
+
+        console.log(encontrado)
+        return encontrado;
+
+    }
+
+
+
+    if (!localStorage.getItem("listaUsuarios")) {
+        return false;
+    }
+
+
+    let almacenados = JSON.parse(localStorage.getItem("listaUsuarios"));
+    let encontrado = false;
+
+    let i = 0;
+
+    while (!encontrado && i != almacenados.length) {
+
+
+        if (almacenados[i].user == usuario) {
+            encontrado = almacenados[i];
+
+        }
+
+        i++;
+
+    }
+
+    return encontrado;
 
 }
 
-}
-
-const Persona1 = new Persona('Igna',24,'No hay mal que por bien no venga');
 
 
-localStorage.setItem('Primeraxd','Hola Scoly');
 
-let Primer = localStorage.getItem('Primeraxd')
 
-console.log(Primer);
+
+    // let almacenados = JSON.parse(localStorage.getItem("listaUsuarios"));
+    // let encontrado = false;
+
+    // let i = 0;
+
+    // while (!encontrado && i != almacenados.length) {
+
+
+    //     if(almacenados[i].user == usuario){
+    //         encontrado = almacenados[i];
+
+    //     }
+
+    //     i++;
+
+    // }
+
+    // return encontrado;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// hablar() {
+//     console.log('Hola soy ' + this.nombre + ' tengo '+this.edad +' años. Y me gusta mucho la frase "'+this.frase+'"')
+
+// }
+
+// }
+
+// const Persona1 = new Persona('Igna',24,'No hay mal que por bien no venga');
+
+
+// localStorage.setItem('Primeraxd','Hola Scoly');
+
+// let Primer = localStorage.getItem('Primeraxd')
+
+// console.log(Primer);
 
 
 
--- registro.js
+++ registro.js
@@ -1,8 +1,116 @@
-function registrar(){
+console.log(localStorage.getItem("listaUsuarios"));
 
+
+class Usuario{
+
+    constructor(user,pass){
+        this.user = user;
+        this.pass = pass;
+    }
+
+}
+
+
+function Registrar(){
+
+  
     let user = document.getElementById("userRegistro").value;
     let pass = document.getElementById("passRegistro").value;
 
-    alert(user+pass)
 
+    let msj ="";
+    if (user=="" || pass=="") {
+        msj = "Falta llenar campos!"
+
+    }
+    else{
+        
+        if (user.length < 4) {
+            
+            msj ="El usuario debe tener almenos 4 caracteres "
+            
+            if (pass.length < 4) {
+                msj = msj + "\nLa contraseña debe tener almenos 4 caracteres."
+                
+            }
+        }
+        else if(buscarUsuario(user) == false){
+            let NewUser = new Usuario(user,pass);
+            GuardarUsuarios(NewUser);
+            msj ="Registro completo!";
+
+
+        }
+        else{
+            msj ='Ya existe ese usuario';
+
+        }
+        
+
+        
+
+    }
+
+    alert(msj);
+    
+
+}
+
+function GuardarUsuarios(usuarioNuevo) {
+
+    let item = localStorage.getItem("listaUsuarios");
+    
+    if (item) {
+        
+        let almacenados = JSON.parse(localStorage.getItem("listaUsuarios"));
+        almacenados.push(usuarioNuevo);
+
+        let almacenados_string = JSON.stringify(almacenados);
+        localStorage.setItem("listaUsuarios",almacenados_string);
+
+    }
+    
+    else{
+
+        let almacenados = new Array();
+        almacenados.push(usuarioNuevo);
+        let almacenados_string = JSON.stringify(almacenados);
+        localStorage.setItem("listaUsuarios",almacenados_string);
+
+
+    }
+
+
+    
+}
+
+
+function buscarUsuario(usuario) {
+
+    if(!localStorage.getItem("listaUsuarios")){
+            return false;
+    }
+
+    
+    let almacenados = JSON.parse(localStorage.getItem("listaUsuarios"));
+    let encontrado = false;
+
+    let i = 0;
+    
+    while (!encontrado && i != almacenados.length) {
+
+        
+        if(almacenados[i].user == usuario){
+            encontrado = almacenados[i];
+
+        }
+
+        i++;
+        
+    }
+
+    return encontrado;
+
+
+    
 }
